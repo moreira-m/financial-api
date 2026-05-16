9af05492ed897ba8ae078c0c8f9593932f3a677c
moreira-m <matheussmoreira0612@gmail.com>
2026-05-16 12:42:25 -0300

    feat: create transactions controller with date filters and sync trigger

--- src/main/java/com/moreira/financial_api/repository/TransactionRepository.java
+++ src/main/java/com/moreira/financial_api/repository/TransactionRepository.java
@@ -4,7 +4,12 @@ import com.moreira.financial_api.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     boolean existsByPluggyTransactionId(String pluggyTransactionId);
+
+    List<Transaction> findByDateBetweenOrderByDateDesc(LocalDate startDate, LocalDate endDate);
 }
