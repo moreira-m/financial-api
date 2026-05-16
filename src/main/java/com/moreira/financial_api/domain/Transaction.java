d5de625bf5fd825e5d00859d11a9aa7886596bd2
moreira-m <matheussmoreira0612@gmail.com>
2026-05-15 22:56:51 -0300

    feat: create Transaction entity with relations

---
+++ src/main/java/com/moreira/financial_api/domain/Transaction.java
@@ -0,0 +1,46 @@
+package com.moreira.financial_api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Transaction {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(name = "pluggy_transaction_id", unique = true)
+    private String pluggyTransactionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @Column(name = "created_ad", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
